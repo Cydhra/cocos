5fa17382d1afc8c421f6dc1ffa796342ee49cb1e
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-09-04 14:50:10 +0200

    fix: when i removed the overflow handling of the SIMD implementation in favor of load_or_default i forgot to replace chunks_exact with chunks

--- libcocos/src/vectors/simd.rs
+++ libcocos/src/vectors/simd.rs
@@ -7,11 +7,11 @@ pub fn dot_prod(a: &[f64], b: &[f64]) -> f64 {
     assert_eq!(a.len(), b.len());
 
     a[..]
-        .chunks_exact(VECTOR_SIZE)
+        .chunks(VECTOR_SIZE)
         .map(Simd::<f64, VECTOR_SIZE>::load_or_default)
         .zip(
             b[..]
-                .chunks_exact(VECTOR_SIZE)
+                .chunks(VECTOR_SIZE)
                 .map(Simd::<f64, VECTOR_SIZE>::load_or_default),
         )
         .map(|(a, b)| a * b)
