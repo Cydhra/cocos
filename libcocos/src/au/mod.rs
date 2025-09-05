08185b5ec4f135bdea1e77bd5fae551ff05cde0a
Johannes Hengstler <mail.jhengstler@gmail.com>
2025-09-05 18:22:01 +0200

    simplify au test by using math::cdf

--- libcocos/src/au/mod.rs
+++ libcocos/src/au/mod.rs
@@ -1,7 +1,7 @@
 use crate::BpTable;
-use statrs::distribution::{ContinuousCDF, Normal};
 
 mod math;
+use crate::au::math::cdf;
 
 mod newton;
 pub use newton::estimate_curv_dist_newton;
@@ -13,9 +13,5 @@ pub fn get_au_value(bp_values: &BpTable) -> Result<Vec<f64>, argmin_math::Error>
     let params = estimate_curv_dist_rss(bp_values)?;
     let results = estimate_curv_dist_newton(bp_values, params)?;
 
-    let normal = Normal::new(0.0, 1.0).unwrap();
-    results
-        .iter()
-        .map(|(c, d)| Ok(1.0 - normal.cdf(d - c)))
-        .collect()
+    results.iter().map(|(c, d)| Ok(1.0 - cdf(d - c))).collect()
 }
