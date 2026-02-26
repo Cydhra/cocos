7538a94d263b2309cb631a7617ac4681579658cc
Johannes Hengstler <mail.jhengstler@gmail.com>
2026-02-26 17:02:48 +0100

    runconsel script only runs consel if output file does not exist

--- bench/scripts/runconsel.sh
+++ bench/scripts/runconsel.sh
@@ -16,15 +16,17 @@ chmod u+x catpv
 sitelh=$1
 prefix=$2
 
-./makermt --puzzle $sitelh "$prefix" || clean_exit 1
-./consel "$prefix.rmt" "$prefix" --no_sort --no_bp --no_pp --no_sh || clean_exit 2
+if [ ! -e "$prefix.csv" ]; then
+  ./makermt --puzzle $sitelh "$prefix" || clean_exit 1
+  ./consel "$prefix.rmt" "$prefix" --no_sort --no_bp --no_pp --no_sh || clean_exit 2
 
-echo "rank,item,obs,au,np" > "$prefix.csv"
-(./catpv "$prefix.pv" || clean_exit 3) \
-  | awk 'NF' \
-  | tail -n +3 \
-  | awk '{
-      printf "%d,%d,%s,%s,%s\n", $2, $3, $4, $5, $6
-    }' >> "$prefix.csv"
+  echo "rank,item,obs,au,np" > "$prefix.csv"
+  (./catpv "$prefix.pv" || clean_exit 3) \
+    | awk 'NF' \
+    | tail -n +3 \
+    | awk '{
+        printf "%d,%d,%s,%s,%s\n", $2, $3, $4, $5, $6
+      }' >> "$prefix.csv"
+fi
 
 clean_exit 0
